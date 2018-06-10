43a185b3adb364869a86e4254a8d3701b61f32a7
harryi <harry59lauren@gmail.com>
2018-06-09 20:37:35 -0400

    first

---
+++ IntegerToTextTranslator.h
@@ -0,0 +1,17 @@
+
+#include <string>
+#include <vector>
+
+#ifndef IntegerToTextTranslator_H
+#define IntegerToTextTranslator_H
+
+class IntegerToTextTranslator{
+
+public :
+    static std::string numberToText(int n){ 
+        return "stub";
+    }
+
+};
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "IntegerToTextTranslator.cpp"
+
+
+int main(int argc, char * argv[]){
+    
+      printf("%s\n", IntegerToTextTranslator::numberToText(123).c_str());
+
+}
+
+
+
+
