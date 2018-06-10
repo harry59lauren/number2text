67807a0c7ecf949cf55793d558cb050f65aae9c3
Marcell Purham <marcellpurham@gmail.com>
2018-06-09 19:51:13 -0500

    removed header file

--- IntegerToTextTranslator.cpp
+++ IntegerToTextTranslator.cpp
@@ -1,1 +1,16 @@
-#include "IntegerToTextTranslator.h"
+#include <string>
+#include <vector>
+
+#ifndef IntegerToTextTranslator_H
+#define IntegerToTextTranslator_H
+
+class IntegerToTextTranslator{
+
+public :
+    static std::string numberToText(int n){ 
+        return "stub";
+    }
+
+};
+
+#endif 
